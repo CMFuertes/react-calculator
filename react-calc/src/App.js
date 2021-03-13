b5d73c6f394c74176aed51ddad27c6aacdb7122d
Cristhal Fuertes <cristhal.fuertes@gmail.com>
2021-03-12 22:39:21 -0500

    added font style

--- react-calc/src/App.js
+++ react-calc/src/App.js
@@ -2,24 +2,24 @@ import React from 'react';
 const app = () => <div className="app"></div>;
 export default app; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
