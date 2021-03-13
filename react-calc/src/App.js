2078ecb58ba4266c799ed2b60bd68bbab75c4718
Cristhal Fuertes <cristhal.fuertes@gmail.com>
2021-03-13 03:47:44 -0500

    imported Calculator

--- react-calc/src/App.js
+++ react-calc/src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import './styles/styles.css';
-const app = () => <div className="app"></div>;
+
+import Calculator from "./layout/Calculator/Calculator";
+
+
+const app = () => ( <div className="app">
+    <Calculator /> 
+</div>
+) ; 
 export default app; 
 
 // function App() {
