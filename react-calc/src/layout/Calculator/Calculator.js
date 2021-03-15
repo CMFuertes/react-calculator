54356cb78fde98254a8f6210002c337b425a3eb6
Cristhal Fuertes <cristhal.fuertes@gmail.com>
2021-03-15 17:24:57 -0400

    implemented onButtonPress and clear method

--- react-calc/src/layout/Calculator/Calculator.js
+++ react-calc/src/layout/Calculator/Calculator.js
@@ -9,6 +9,39 @@ class Calculator extends React.Component {
         equation: '', 
         result: 0
     }
+
+        onButtonpress = event => {
+            let equation = this.station.equation;
+            const pressedButton = event.target.innerHTML;
+
+            if (pressedButton === 'C') return this.clear();
+
+            if (pressedBUtton === 'C' ) return this.clear();
+            else if ((pressedButton >= '0' && pressedButton <= '9') || pressedButton === '.') equation += pressedButton; 
+            else if (['+', '-', '*', '/', '%'].indexOf(pressedButton) !== -1) equation += ' '
++ pressedButton + ' ';
+else if (pressedButton === '=') {
+    try {
+        const evalResult = eval(equation);
+        const result = Number.isInteger(evalResult) ?
+        evalResult : evalResult.toFixed(2) ;
+        this.setState({result});
+    } catch (error) {
+        alert('Invalid Mathematical Equation');
+        }
+    }
+    else {
+        equation = equation.trim();
+        equation = equation.substr(0, equation.length - 1);
+    }
+    
+        this.setState({equation: equation});
+    
+}       
+clear () {
+    this.setState({equation: equation});
+}
+
     render() {
         return (
 
