395ec96795b551ce2bc8007b4e70eac6ccf9dcf8
Cristhal Fuertes <cristhal.fuertes@gmail.com>
2021-03-14 20:45:26 -0400

    changed buttons

--- react-calc/src/layout/Calculator/Keypad/Keypad.js
+++ react-calc/src/layout/Calculator/Keypad/Keypad.js
@@ -13,28 +13,29 @@ const keypad = () => (
 
         </KeypadRow>
         <KeypadRow>
-            <Button />
-            <Button />
-            <Button />
-            <Button />
+            <Button>9</Button>
+            <Button>8</Button>
+            <Button>7</Button>
+            <Button>*</Button>
+        </KeypadRow>
+
+        <KeypadRow>
+            <Button>6</Button>
+            <Button>5</Button>
+            <Button>4</Button>
+            <Button>-</Button>
         </KeypadRow>
         <KeypadRow>
-            <Button />
-            <Button />
-            <Button />
-            <Button />
+            <Button>3</Button>
+            <Button>2</Button>
+            <Button>1</Button>
+            <Button>+</Button>
         </KeypadRow>
         <KeypadRow>
-            <Button />
-            <Button />
-            <Button />
-            <Button />
-        </KeypadRow>
-        <KeypadRow>
-            <Button />
-            <Button />
-            <Button />
-            <Button />
+            <Button>0</Button>
+            <Button>.</Button>
+            <Button>=</Button>
+            <Button></Button>
             </KeypadRow>
     </section>
 );
