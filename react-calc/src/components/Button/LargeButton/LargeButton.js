440c0f230e573dcdc5c915c17a19692cf8517bec
Cristhal Fuertes <cristhal.fuertes@gmail.com>
2021-03-16 23:41:12 -0400

    updated props

--- react-calc/src/components/Button/LargeButton/LargeButton.js
+++ react-calc/src/components/Button/LargeButton/LargeButton.js
@@ -3,6 +3,6 @@ import React from 'react';
 import Button from '../Button';
 
 const largeButton = (props) => <Button type='large'>
-    {props.children}</Button>;
+    {...props}</Button>;
 
 export default largeButton;
